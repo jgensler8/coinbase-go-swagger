18e38f7f308bd9d46f3c59d7a0590e4e3514a2ae
Jeffrey Gensler <jgensler8@gmail.com>
2020-09-13 21:37:45 -0700

    add ability to place orders

--- orders.go
+++ orders.go
@@ -1,1 +1,16 @@
 package coinbase
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/jgensler8/coinbase-go-swagger/swagger"
+)
+
+func (c *Client) CreateOrder(ctx context.Context, order swagger.OrderRequest) ([]swagger.Order, *http.Response, error) {
+	timestamp, header, err := generateSignHeader(c.secret, "POST", "/orders", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	return c.swaggerClient.OrdersApi.OrdersPost(ctx, c.key, header, timestamp, c.passphrase, order)
+}
