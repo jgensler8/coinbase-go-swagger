5affd7801b794e6d800b5735601fe35a1f713faa
Jeffrey Gensler <jgensler8@gmail.com>
2020-09-13 18:55:16 -0700

    didn't update path but this was somehow working in stage...

--- products.go
+++ products.go
@@ -2,6 +2,7 @@ package coinbase
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/jgensler8/coinbase-go-swagger/swagger"
@@ -16,7 +17,7 @@ func (c *Client) GetTrades(ctx context.Context, productId string) ([]swagger.Tra
 }
 
 func (c *Client) GetHistoricRates(ctx context.Context, productId string, parameters swagger.HistoricRateRequest) ([]swagger.Candle, *http.Response, error) {
-	timestamp, header, err := generateSignHeader(c.secret, "GET", "/accounts", nil)
+	timestamp, header, err := generateSignHeader(c.secret, "GET", fmt.Sprintf("/products/%s/candles", productId), nil)
 	if err != nil {
 		return nil, nil, err
 	}
